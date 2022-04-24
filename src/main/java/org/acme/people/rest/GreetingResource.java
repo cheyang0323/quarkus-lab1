b9b93d4fb0f018d50f9fbaf524535026b0cc182e
David <cheyang@redhat.com>
2022-04-24 20:01:31 +0800

    update

--- src/main/java/org/acme/people/rest/GreetingResource.java
+++ src/main/java/org/acme/people/rest/GreetingResource.java
@@ -1,5 +1,7 @@
 package org.acme.people.rest;
 
+import java.util.Optional;
+
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -8,6 +10,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import org.acme.people.service.GreetingService;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,6 +23,15 @@ public class GreetingResource {
 
     @Inject
     GreetingService service;
+
+    @ConfigProperty(name = "greeting.message")
+    String message;
+
+    @ConfigProperty(name = "greeting.suffix", defaultValue = "!")
+    String suffix;
+
+    @ConfigProperty(name = "greeting.name")
+    Optional<String> name;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
@@ -34,6 +46,16 @@ public class GreetingResource {
     @Produces(MediaType.TEXT_PLAIN)
     @NonBlocking
     public String hello() {
-        return "hello";
+        return message + " " + name.orElse("world") + suffix;
+    }
+
+    @GET
+    @Path("/lastletter/{name}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String lastLetter(@PathParam("name") String name) {
+        int len = name.length();
+        String lastLetter = name.substring(len - 1);
+        log.info("Got last letter: " + lastLetter);
+        return lastLetter;
     }
 }
--- src/test/java/org/acme/people/GreetingResourceTest.java
+++ src/test/java/org/acme/people/GreetingResourceTest.java
@@ -1,8 +1,15 @@
 package org.acme.people;
 
 import io.quarkus.test.junit.QuarkusTest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.acme.people.service.GreetingService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import java.util.UUID;
+
+import javax.inject.Inject;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
@@ -10,15 +17,35 @@ import static org.hamcrest.CoreMatchers.startsWith;
 
 @QuarkusTest
 public class GreetingResourceTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger("GreetingResourceTest");
+
+    @Inject
+    GreetingService service;
+
+    @Test
+    public void testGreetingService() {
+        LOGGER.info("greeting: " + service.greeting("Quarkus"));
+        Assertions.assertTrue(service.greeting("Quarkus").startsWith("hello Quarkus"));
+    }
 
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("hello"));
+                .when().get("/hello")
+                .then()
+                .statusCode(200)
+                .body(startsWith("hello"));
     }
 
     // add more tests
+    @Test
+    public void testGreetingEndpoint() {
+        String uuid = UUID.randomUUID().toString();
+        given()
+                .pathParam("name", uuid)
+                .when().get("/hello/greeting/{name}")
+                .then()
+                .statusCode(200)
+                .body(startsWith("hello " + uuid));
+    }
 }
