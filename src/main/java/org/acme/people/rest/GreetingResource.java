f43912fdcbb97468aad82394222dee357c0b1d06
David <cheyang@redhat.com>
2022-04-27 12:38:24 +0800

    completed

--- src/main/java/org/acme/people/rest/GreetingResource.java
+++ src/main/java/org/acme/people/rest/GreetingResource.java
@@ -14,12 +14,18 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import io.smallrye.common.annotation.NonBlocking;
 
 @Path("/hello")
 public class GreetingResource {
 
     public static final Logger log = LoggerFactory.getLogger(GreetingResource.class);
+    private final MeterRegistry registry;
+
+    GreetingResource(MeterRegistry registry) {
+        this.registry = registry;
+    }
 
     @Inject
     GreetingService service;
@@ -46,6 +52,7 @@ public class GreetingResource {
     @Produces(MediaType.TEXT_PLAIN)
     @NonBlocking
     public String hello() {
+        registry.counter("greeting.hello.counter").increment();
         return message + " " + name.orElse("world") + suffix;
     }
 
